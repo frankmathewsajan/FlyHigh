f4070cdcbbf3930af719b47ae53c4d0a651a4a9a
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2024-12-19 19:58:08 +0530

    some changes

--- drone/views.py
+++ drone/views.py
@@ -1,5 +1,19 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from drone.utils import calculate_paths
+
 
 # Create your views here.
 def index(request):
     return render(request, 'drone/index.html')
+
+
+def map_view(request):
+    if request.method == 'POST':
+        coordinates = json.loads(request.POST.get('coordinates', '[]'))
+        mst, drone_path = calculate_paths(coordinates)
+        return JsonResponse({'mst': mst, 'drone_path': drone_path})
+    return render(request, 'drone/mapper.html')
